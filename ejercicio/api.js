a9ede2f18c211cc484716b9c81848b6469466fe0
DavidXie10 <davidxieli@gmail.com>
2021-10-07 11:59:43 -0600

    Finished first exercise

---
+++ ejercicio/api.js
@@ -0,0 +1,89 @@
+
+const COUNTRY_NAME_INPUT = "search"
+const UNIVERSITY_DATA_PLACE = 'data'
+const BASE_URL = 'http://universities.hipolabs.com/search'
+
+async function main(){
+    document.getElementById(COUNTRY_NAME_INPUT).addEventListener("keyup", search);
+
+    const universitiesData = await getAllUniversitiesByCountry("United States")
+
+    if(universitiesData !== null){
+        data = parseDataFromJson(universitiesData)
+        displayData(data)
+    }
+}
+
+async function getAllUniversitiesByCountry (countryName){
+    let universitiesData = null
+    const universityURL = `${BASE_URL}?country=${countryName}`
+    
+    try {
+        const response = await fetch(universityURL)
+        universitiesData = await response.json()
+    } catch (error) {
+        console.log(`Algo ha fallado: ${error.message}`)
+    }
+
+    return universitiesData;
+}
+
+function parseDataFromJson(universitiesData){
+    parsedData = []
+    
+    for(let index = 0; index < universitiesData.length; ++index){
+        let actualUniversitie = universitiesData[index]
+        parsedData.push({ 
+            "id": index,
+            "name": actualUniversitie.name,
+            "country": actualUniversitie.country,
+            "alphaCode": actualUniversitie.alpha_two_code,
+            "webPage": actualUniversitie.web_pages[0]
+        });
+    }
+    
+    return parsedData
+}
+
+function displayData(data){
+    let display = "";
+
+    data.forEach((post) => {
+        display += `
+        <ul id = "${post.id}">
+        <li><strong>University name:</strong> ${post.name} </li>
+        <li><strong>Country:</strong> ${post.country} </li>
+        <li><strong>Alpha two code: </strong>${post.alphaCode}</li>
+        <li><strong>Main web page: </strong><a href="${post.webPage}">${post.webPage}</a></li>
+        </ul>
+        `;
+    });
+
+    document.getElementById(UNIVERSITY_DATA_PLACE).innerHTML = display;
+}
+
+function search(){
+    let text = document.getElementById("search").value;
+    data.forEach((post) => {
+        if (match(post.name, text) || text == "")
+            show(post.id)
+        else
+            hide(post.id)
+    });
+}
+
+function match(word, substring){
+    console.log(word, substring);
+    console.log(word.includes(substring));
+    return word.includes(substring);
+}
+
+function hide(elementId){
+    document.getElementById(elementId).style.display = "none";
+}
+
+function show(elementId){
+    document.getElementById(elementId).style.display = "block";
+}
+
+main()
